440b86d73dd59f4906ff1830cecea5bd4fdecf70
Pratyush <pratyushverma@outlook.com>
2016-10-17 22:04:51 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+WeSiSho
+===
+
+<b>We</b>b <b>Si</b>tes <b>Sho</b>rtcuts is a chrome extension for adding shortcuts inside websites. 
+
+
+### Build
+> npm run build
+
+##### Work in Progress
+
+#### TODOs
+
+- Get rid of shortcut.js
+- Add a settings page
+- Add a way to record actions
+
---
+++ webpack.config.babel.js
@@ -0,0 +1,26 @@
+import path from 'path';
+
+export default {
+    module: {
+        loaders: [
+            {
+                test: /\.(?:js|es6)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            }
+        ]
+    },
+    entry: {
+        app :  path.join(__dirname, "lib", "app"),
+        background : path.join(__dirname, "lib", "background")
+    },
+    devtool: 'source-map',
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].js",
+    },
+    resolve : {
+        extensions: ["", ".js", ".es6"],
+        packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
+    }
+};
