a70d8aea0e34c34968174271ea38496f16ad4f9d
Pratyush <pratyushverma@outlook.com>
2018-03-09 00:16:29 +0530

    Get rid of version file plugin

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,7 +1,6 @@
 const _ = require('lodash');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const VersionFilePlugin = require('webpack-version-file-plugin');
 
 const config = require('./config.js');
 
@@ -16,14 +15,9 @@ module.exports = _.merge({}, config, {
       { from: './node_modules/bootstrap/dist/', to: './bootstrap/' },
       { from: './node_modules/sweetalert2/dist/sweetalert2.min.css', to: './css/' },
     ], {
-      ignore: ['js/**/*', 'manifest.json'],
-      copyUnmodified: false
+      ignore: ['js/**/*'],
+      copyUnmodified: false,
     }),
-    new VersionFilePlugin({
-      packageFile: path.resolve(__dirname, '../package.json'),
-      template: path.resolve(__dirname, '../src/manifest.json'),
-      outputFile: path.resolve(__dirname, '../build/dev/manifest.json'),
-    })
   ],
-  watch: true
+  watch: true,
 });
