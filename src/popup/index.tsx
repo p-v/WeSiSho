5ab3059bbbc8fe29ae575604f4f80b5d80215252
Pratyush <pratyushverma@outlook.com>
2020-06-20 21:46:37 +0530

    Migration popup

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {Component, createRef} from 'react';
 import { render } from 'react-dom';
 import '../../css/wesisho.global.css';
 import { shortcutInput, shortcutDescription, saveButton, optionContainer, cancelButton } from './style.css';
 
 const SHORTCUT_EMPTY = 'Oops! Looks like no shortcut has been set';
 
-class Main extends React.Component {
+type PopupState = {
+  addShortcut: boolean;
+  createShortcut: boolean;
+  recording: boolean;
+  selectors: string[];
+  refUrl: string;
+}
+
+class Main extends Component<unknown, PopupState> {
+
+  private refUrlInput = createRef<HTMLInputElement>();
+  private shortcutInput = createRef<HTMLInputElement>();
+  private shortcutDescInput = createRef<HTMLTextAreaElement>();
 
   constructor() {
-    super();
+    super({});
     this.state = {
       addShortcut: false,
       createShortcut: false,
       recording: false,
       selectors: [],
+      refUrl: '',
     };
     this.onAddButtonClick = this.onAddButtonClick.bind(this);
     this.onSaveShortcutClick = this.onSaveShortcutClick.bind(this);
@@ -40,12 +53,18 @@ class Main extends React.Component {
   onSaveShortcutClick() {
     this.setState({ addShortcut: false });
 
-    const keyValue = this.shortcutInput.value;
-    const description = this.shortcutDescInput.value;
+    if (!this.shortcutInput.current || !this.shortcutDescInput.current) {
+      return;
+    }
+
+    const keyValue = this.shortcutInput.current.value;
+    const description = this.shortcutDescInput.current.value;
 
     if (!keyValue) {
       chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'error', message: SHORTCUT_EMPTY });
+        if (tabs[0].id) {
+          chrome.tabs.sendMessage(tabs[0].id, { action: 'error', message: SHORTCUT_EMPTY });
+        }
       });
       return;
     }
@@ -55,17 +74,24 @@ class Main extends React.Component {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
       const url = tabs[0].url;
       const key = keyValue;
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'save_url', url, key, description });
+      if (tabs[0].id) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'save_url', url, key, description });
+      }
     });
   }
 
   onSaveRecordingClick() {
-    const key = this.shortcutInput.value;
-    const description = this.shortcutDescInput.value;
+    if (!this.shortcutInput.current || !this.shortcutDescInput.current) {
+      return;
+    }
+    const key = this.shortcutInput.current.value;
+    const description = this.shortcutDescInput.current.value;
 
     if (!key) {
       chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'error', message: SHORTCUT_EMPTY });
+        if (tabs[0].id) {
+          chrome.tabs.sendMessage(tabs[0].id, { action: 'error', message: SHORTCUT_EMPTY });
+        }
       });
       return;
     }
@@ -77,13 +103,15 @@ class Main extends React.Component {
       createShortcut: false,
       recording: false,
       selectors: [],
-      refUrl: undefined,
+      refUrl: '',
     });
 
     window.close(); // Close extension popup
 
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'save_recording', key, description, refUrl, selectors });
+      if (tabs[0].id) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'save_recording', key, description, refUrl, selectors });
+      }
     });
   }
 
@@ -114,11 +142,12 @@ class Main extends React.Component {
   }
 
   onCancelSaveShortcutClick() {
-    this.setState({ createShortcut: false, selectors: [], refUrl: undefined });
+    this.setState({ createShortcut: false, selectors: [], refUrl: '' });
   }
 
-  onRefUrlChange(e) {
-    this.setState({ refUrl: e.target.value });
+  onRefUrlChange(e: React.FormEvent) {
+    const target = e.target as HTMLInputElement;
+    this.setState({ refUrl: target.value });
   }
 
   renderView() {
@@ -126,65 +155,61 @@ class Main extends React.Component {
 
     if (selectors.length > 0) {
       const selectorRows = selectors.map((selector, idx) =>
-        <div key={idx}>{selector}</div>
-       );
+      <div key={idx}>{selector}</div>
+      );
 
       return (
         <div>
           <h3>{'Save following sequence?'}</h3>
-          {selectorRows}
-          <input
-            ref={(refUrlInput) => { this.refUrlInput = refUrlInput; }}
-            type="text"
-            className={shortcutInput}
-            placeholder="Reference page"
-            autoFocus
-            onChange={this.onRefUrlChange}
-            value={refUrl}
-          />
-          <input
-            ref={(shortcutInputV) => { this.shortcutInput = shortcutInputV; }}
-            type="text"
-            maxLength={10}
-            className={shortcutInput}
-            placeholder="Set shortcut for the sequence"
-            autoFocus
-          />
-          <textarea
-            ref={(shortcutDescInput) => { this.shortcutDescInput = shortcutDescInput; }}
-            className={shortcutDescription}
-            type="text"
-            placeholder="Add description for the sequence"
-          />
-          <button className={saveButton} onClick={this.onSaveRecordingClick}>{'Save'}</button>
-          <button className={cancelButton} onClick={this.onCancelSaveShortcutClick}>{'Cancel'}</button>
-        </div>
-      );
+        {selectorRows}
+        <input
+          ref={this.refUrlInput}
+          type="text"
+          className={shortcutInput}
+          placeholder="Reference page"
+          autoFocus
+          onChange={this.onRefUrlChange}
+          value={refUrl}
+        />
+        <input
+          ref={this.shortcutInput}
+          maxLength={10}
+          className={shortcutInput}
+          placeholder="Set shortcut for the sequence"
+          autoFocus
+        />
+        <textarea
+          ref={this.shortcutDescInput}
+          className={shortcutDescription}
+          placeholder="Add description for the sequence"
+        />
+        <button className={saveButton} onClick={this.onSaveRecordingClick}>{'Save'}</button>
+        <button className={cancelButton} onClick={this.onCancelSaveShortcutClick}>{'Cancel'}</button>
+      </div>
+    );
     }
 
     if (!createShortcut) {
       return (
         <div className={optionContainer}>
           <button onClick={this.onRecordClick}>{recording ? 'Stop Recording' : 'Record'}</button>
-          { !recording && <button onClick={this.onCreateShortcutClick}>{'Create Shortcut'}</button> }
-        </div>
-      );
+        { !recording && <button onClick={this.onCreateShortcutClick}>{'Create Shortcut'}</button> }
+      </div>
+    );
     }
 
     return (
       <div>
         <input
-          ref={(shortcutInputV) => { this.shortcutInput = shortcutInputV; }}
-          type="text"
+          ref={this.shortcutInput}
           maxLength={10}
           className={shortcutInput}
           placeholder="Set shortcut for page"
           autoFocus
         />
         <textarea
-          ref={(shortcutDescInput) => { this.shortcutDescInput = shortcutDescInput; }}
+          ref={this.shortcutDescInput}
           className={shortcutDescription}
-          type="text"
           placeholder="Add description for shortcut"
         />
         <button className={saveButton} onClick={this.onSaveShortcutClick}>{'Save'}</button>
